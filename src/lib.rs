877eee3d123f9f18e726503230514ebfe6c67a17
livinginthegreyzone <livinginthegreyzone@gmail.com>
2022-11-17 11:16:21 +0530

    switch to slash commands method of interaction with bot

--- src/lib.rs
+++ src/lib.rs
@@ -1,36 +1,58 @@
+mod commands;
+
 use serenity::{
     async_trait,
     client::{Context, EventHandler},
-    model::{channel::Message, gateway::Ready},
+    model::{
+        gateway::Ready,
+        id::GuildId,
+        prelude::interaction::{Interaction, InteractionResponseType},
+    },
 };
 
 pub struct Handler;
 
 #[async_trait]
 impl EventHandler for Handler {
-    // Set a handler for the `message` event - so that whenever a new
-    // message is received - the closure (or function) passed will be
-    // called.
-    //
-    // Event handlers are dispatched through a threadpool, and so
-    // multiple events can be dispatched simultaneously.
-    async fn message(&self, ctx: Context, msg: Message) {
-        if msg.content == "!ping" {
-            // Sending a message can fail, due to a network error, an
-            // authentication error, or lack of permissions to post in
-            // the channel, so log to stdout when some error happens,
-            // with a description of it.
-            if let Err(why) = msg.channel_id.say(&ctx.http, "Pong!").await {
-                println!("Error sending message: {:?}", why);
-            }
-        }
-    }
-
     // Set a handler to be called on the `ready` event. This is called
     // when a shard is booted, and a READY payload is sent by
     // Discord. This payload contains data like the current user's
     // guild Ids, current user data, private channels, and more.
-    async fn ready(&self, _: Context, ready: Ready) {
-        println!("{} is connected!", ready.user.name);
+    async fn ready(&self, ctx: Context, ready: Ready) {
+        println!("info: {} is connected!", ready.user.name);
+
+        for guild_id in ctx.cache.guilds().iter() {
+            let commands = GuildId::set_application_commands(guild_id, &ctx.http, |commands| {
+                commands.create_application_command(|command| commands::ping::register(command))
+            })
+            .await;
+
+            println!(
+                "I now have the following guild slash commands: {:#?}",
+                commands
+            );
+        }
+    }
+
+    async fn interaction_create(&self, ctx: Context, interaction: Interaction) {
+        if let Interaction::ApplicationCommand(command) = interaction {
+            println!("info: received command interaction: {:#?}", command);
+
+            let content = match command.data.name.as_str() {
+                "ping" => commands::ping::run(&command.data.options),
+                _ => "not implemented :(".to_string(),
+            };
+
+            if let Err(why) = command
+                .create_interaction_response(&ctx.http, |response| {
+                    response
+                        .kind(InteractionResponseType::ChannelMessageWithSource)
+                        .interaction_response_data(|message| message.content(content))
+                })
+                .await
+            {
+                println!("Cannot respond to slash command: {}", why);
+            }
+        }
     }
 }
